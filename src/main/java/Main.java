25bc1527e2f412f6e3c80dd65cb607a281fbadbb
Parjal Rai <parjal.rai@dvla.gov.uk>
2024-12-16 21:07:48 +0000

    Create BookingRental Class

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,10 @@
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.Properties;
 import java.util.Scanner;
 
 public class Main {
@@ -5,6 +12,18 @@ public class Main {
     public static Scanner stdin = new Scanner(System.in);
 
     public static void main(String[] args) {
+        String connectionString = "jdbc:postgresql://localhost:5432/CarRental";
+        Properties props = new Properties();
+
+        try {
+            InputStream input = Main.class.getClassLoader().getResourceAsStream("db.properties");
+            props.load(input);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        BookingRental.viewAllBookingRentals(connectionString,props);
+
 
     }
 }
