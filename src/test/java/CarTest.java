ffda524807908a57c7b78f0988ca485831c58c42
Jayvibeee <jeffery.aideyan-ohonba@dvla.gov.uk>
2024-12-18 17:46:12 +0000

    Add unit test files for Car and Location class

---
+++ src/test/java/CarTest.java
@@ -0,0 +1,17 @@
+//import org.junit.jupiter.api.Test;
+//import org.mockito.Mock;
+//
+//import static org.mockito.Mockito.*;
+//
+//import java.sql.*;
+//import static org.junit.jupiter.api.Assertions.*;
+//
+//
+//public class CarTest {
+//
+//
+//    @Test
+//    public static void viewAllCars()_
+//
+//
+//}
