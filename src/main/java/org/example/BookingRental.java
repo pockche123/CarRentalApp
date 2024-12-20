2ef5a3df8489f17c1de61e23a7e19430ba471027
Parjal Rai <parjal.rai@dvla.gov.uk>
2024-12-20 12:08:53 +0000

    delete unused function

--- src/main/java/org/example/BookingRental.java
+++ src/main/java/org/example/BookingRental.java
@@ -62,40 +62,6 @@ public class BookingRental{
 
 
 
-    public static void viewAllBookingRentals() {
-
-        System.out.println("Please enter the booking ID:");
-        try (Connection conn = Main.establishConnection()){
-
-            Statement stmt = conn.createStatement();
-
-            // Query to select all data from the booking_rentals table
-            ResultSet rs = stmt.executeQuery("SELECT * FROM booking_rentals");
-
-            // Print a header for the output
-            System.out.println("Booking Rental ID | \tPayment ID |\tRegistration Plate |\tDropoff Location ID | \tCustomer ID | \tSuspend");
-
-            // Process each row in the result set
-            while (rs.next()) {
-                // Retrieve each column value
-                int bookingRentalId = rs.getInt("booking_rental_id");
-                int paymentId = rs.getInt("payment_id");
-                String registrationPlate = rs.getString("registration_plate");
-                int dropoffLocationId = rs.getInt("dropoff_location_id");
-                int customerId = rs.getInt("customer_id");
-                boolean suspend = rs.getBoolean("suspend");
-
-                // Print the values to the console
-                System.out.printf("\t\t%d\t\t\t\t\t%d\t\t\t\t%s\t\t\t\t\t\t\t%d\t\t\t\t%d\t\t\t%s\n",
-                        bookingRentalId, paymentId, registrationPlate, dropoffLocationId, customerId, suspend);
-            }
-
-        } catch (SQLException e) {
-            e.printStackTrace();
-            throw new RuntimeException(e);
-        }
-    }
-
     public static void viewAllBookingRentalsBySuspend(boolean suspendStatus) {
 
         System.out.println("Please enter the booking ID:");
